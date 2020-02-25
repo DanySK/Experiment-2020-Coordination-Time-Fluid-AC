23bce2a34b3cdff6f9ccdad064266752d5df6e90
Danilo Pianini <danilo.pianini@unibo.it>
2020-02-25 15:01:51 +0100

    [EXPERIMENT] Delete unused variables

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,7 +94,7 @@ File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
             args(
                 "-e", "data/${it.nameWithoutExtension}",
                 "-b",
-                "-var", "seed", "speed", "meanNeighbors", "nodeCount",
+                "-var", "seed", "speed",
                 "-p", threadCount,
                 "-i", 1
             )
